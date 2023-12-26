6458712e432eb5e7bad186e8333c4be1e6a74145
adityagoel28 <adityagoel2002@gmail.com>
2023-12-26 20:26:01 +0530

    Added Delete First element method

--- src/main/java/LinkedList/Main.java
+++ src/main/java/LinkedList/Main.java
@@ -26,5 +26,10 @@ public class Main {
         list.insertAfter(list.head, 30);
         System.out.println("\nLinkedList after inserting 30 between 56 and 70:");
         list.printList();
+
+        // Use Case 5
+        list.pop();
+        System.out.println("\nLinkedList after deleting the first element:");
+        list.printList();
     }
 }
